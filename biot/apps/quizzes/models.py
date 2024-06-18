26cde3c2b433b5d99b542ac7906f697e4cf25684
Practic73 <sierrachart@yandex.ru>
2024-06-18 11:07:27 +0300

    clear code

--- biot/apps/quizzes/models.py
+++ biot/apps/quizzes/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Quiz(models.Model):
     name = models.CharField(max_length=120)
     published = models.DateTimeField(auto_now_add=True)
-     
+
     def __str__(self):
         return self.name
 
@@ -14,16 +14,17 @@ class Quiz(models.Model):
         verbose_name = 'Тест'
         verbose_name_plural = 'Тесты'
 
+
 class Question(models.Model):
     class qtype(models.TextChoices):
         single = 'single'
         multiple = 'multiple'
-   
+
     name = models.CharField(max_length=350)
-    qtype = models.CharField(max_length=8, choices=qtype.choices, default=qtype.single)
+    qtype = models.CharField(max_length=8, choices=qtype.choices,
+                             default=qtype.single)
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     explanation = models.CharField(max_length=550)
-
 
     def get_answers(self):
         if self.qtype == 'single':
@@ -31,7 +32,6 @@ class Question(models.Model):
         else:
             qs = self.answer_set.filter(is_correct=True).values()
             return [i.get('name') for i in qs]
-
 
     def user_can_answer(self, user):
         user_choices = user.choice_set.all()
@@ -49,6 +49,7 @@ class Question(models.Model):
         verbose_name = 'Вопрос'
         verbose_name_plural = 'Вопросы'
 
+
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
@@ -61,16 +62,15 @@ class Answer(models.Model):
         verbose_name = 'Ответ'
         verbose_name_plural = 'Ответы'
 
+
 class Choice(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
-     
+
+
 class Result(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     correct = models.IntegerField(default=0)
     wrong = models.IntegerField(default=0)
-
-
-
